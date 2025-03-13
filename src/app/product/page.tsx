1203b7949f8a1847b4ece08537faf9623f881545
xeff09 <wongchaita03@gmail.com>
2025-03-13 17:24:56 +0700

    init: all getting prods usage

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,26 +1,43 @@
 "use client";
+import Image from "next/image";
 import React from "react";
 import { products } from "@/mock-data/products";
 import { Product } from "@/interfaces/Product";
 import Link from "next/link";
+import { useGetBuyerProductRequests } from "@/api/productRequest/useProductRequest";
+import { GetProductRequestResponseDTO } from "@/dtos/productRequest";
 
-const ProductList: React.FC<{ products: Product[] }> = ({ products }) => {
+const ProductList: React.FC<{ products: Product[] }> = () => {
+  const { data: prods3, isLoading: loadingProds3 } =
+    useGetBuyerProductRequests();
+
+  if (loadingProds3) {
+    return <div>..Loading</div>;
+  }
+
   return (
     <div className="overflow-x-auto p-4">
       <div className="grid grid-cols-2 gap-6">
-        {products.map((product) => (
-          <Link key={product.id} href={`/product/${product.id}`} passHref>
-            <div className="border border-gray-200 p-4 rounded-lg shadow-md bg-white cursor-pointer">
-              <img
-                src={product.image}
-                alt={product.name}
-                className="w-full h-full object-cover rounded-md"
-              />
-              <h2 className="text-lg font-semibold mt-2">{product.name}</h2>
-              <p className="text-gray-600 text-sm">{product.description}</p>
-            </div>
-          </Link>
-        ))}
+        {prods3!["product-requests"].map(
+          (product: GetProductRequestResponseDTO) => (
+            <Link key={product.id} href={`/product/${product.id}`} passHref>
+              <div className="border border-gray-200 p-4 rounded-lg shadow-md bg-white cursor-pointer">
+                <Image
+                  src={product.images[0]}
+                  alt={product.name || "Product Request Image"}
+                  width={180}
+                  height={180}
+                  className="w-full h-full object-cover rounded-md"
+                />
+                <h2 className="text-lg font-semibold mt-2">{product.name}</h2>
+                <p className="text-gray-600 text-sm">{product.desc}</p>
+                <p className="text-gray-600 text-sm">{product.from}</p>
+                <p className="text-gray-600 text-sm">{product.to}</p>
+                <p className="text-gray-600 text-sm">{product.budget}</p>
+              </div>
+            </Link>
+          ),
+        )}
       </div>
     </div>
   );
@@ -29,7 +46,9 @@ const ProductList: React.FC<{ products: Product[] }> = ({ products }) => {
 function Page() {
   return (
     <div className="px-8 bg-gray-50 rounded pt-8 pb-8 min-h-screen">
-      <h1 className="text-3xl font-bold text-gray-900 mb-6">My Product Requests</h1>
+      <h1 className="text-3xl font-bold text-gray-900 mb-6">
+        My Product Requests
+      </h1>
       <ProductList products={products} />
     </div>
   );
